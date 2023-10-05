b91259d71e2bc1ef44512fd6de66c1514bbaaa44
Nico Lenz <89878305+Nicnac13@users.noreply.github.com>
2023-10-05 23:34:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let sections = document.querySelectorAll(".section");
+    let currentSection = -1;
+
+    function scrollToSection(index) {
+        sections[index].scrollIntoView({ behavior: "smooth" });
+    }
+
+    window.addEventListener("wheel", function (event) {
+        if (event.deltaY > 0 && currentSection < sections.length - 1) {
+            currentSection++;
+            scrollToSection(currentSection);
+        } else if (event.deltaY < 0 && currentSection > 0) {
+            currentSection--;
+            scrollToSection(currentSection);
+        }
+    });
+});
