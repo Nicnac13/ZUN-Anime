c590a023e3b949dcc84f238ff9baa485e8589f7e
Lukas Doerig <lukee910@gmail.com>
2023-10-07 21:49:08 +0200

    Rework 4koma

--- gallery.js
+++ gallery.js
@@ -1,22 +1,24 @@
-const thumbnails = document.querySelectorAll('.thumbnail');
-const modal = document.querySelector('.modal');
-const modalImage = document.querySelector('#enlarged-image');
-const closeModal = document.querySelector('.close');
-
-thumbnails.forEach(thumbnail => {
-    thumbnail.addEventListener('click', () => {
-        const imageSrc = thumbnail.querySelector('img').src;
-        modalImage.src = imageSrc;
-        modal.style.display = 'block';
+document.addEventListener("DOMContentLoaded", function () {
+    const thumbnails = document.querySelectorAll('.thumbnail');
+    const modal = document.querySelector('.modal');
+    const modalImage = document.querySelector('#enlarged-image');
+    const closeModal = document.querySelector('.close');
+    
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('click', () => {
+            const imageSrc = thumbnail.querySelector('img').src;
+            modalImage.src = imageSrc;
+            modal.style.display = 'block';
+        });
     });
-});
-
-closeModal.addEventListener('click', () => {
-    modal.style.display = 'none';
-});
-
-window.addEventListener('click', (event) => {
-    if (event.target === modal) {
+    
+    closeModal.addEventListener('click', () => {
         modal.style.display = 'none';
-    }
+    });
+    
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
 });
